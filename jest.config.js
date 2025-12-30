a7067e7c1afb7e0f9a1c966ea1dd9b0c28a6e0a5
Preston McCormick <Preston.McCormick@aiworldwide.com>
2025-12-29 21:18:49 -0700

    Fix test broken after fixing deployed image... had to use a mock, seems kind of jank

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
   moduleNameMapper: {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/__mocks__/fileMock.js",
   },
   transform: {
     "^.+\\.(js|jsx)$": [
